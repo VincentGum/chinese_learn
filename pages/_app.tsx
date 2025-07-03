64cc5a3df925b9ae0233a792d51a879d909caa46
甘伟森 <ganweisen@bytedance.com>
2025-07-03 13:44:54 +0800

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
+import '../globals.css';
 import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
